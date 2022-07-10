9e375afba1d3e40d6d961c4bf526b278a615a343
pasly0920 <inwoo920621@gmail.com>
2022-07-10 23:25:48 +0900

    [Env] Webpack Bundling

---
+++ 07_July/pasly0920/config/webpack.common.js
@@ -0,0 +1,31 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+const webpack = require('webpack');
+// const { BundleAnalyzerPlugin } = require("webpack-bundle-analyzer");
+
+module.exports = {
+  entry: `${path.resolve(__dirname, '../src')}/index.tsx`,
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx|js|jsx)$/,
+        use: 'babel-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: `${path.resolve(__dirname, '../public')}/index.html`,
+    }),
+    new webpack.ProvidePlugin({
+      React: 'react',
+    }),
+  ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, '../src/'),
+    },
+    extensions: ['.js', '.ts', '.jsx', '.tsx', '.css', '.json'],
+  },
+};
---
+++ 07_July/pasly0920/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import Hello from '@/component/Hello';
+
+const App = () => {
+  return (
+    <div>
+      Hello World
+      <Hello />
+    </div>
+  );
+};
+export default App;
